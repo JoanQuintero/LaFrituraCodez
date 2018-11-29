79819c814b2987d09cc0f5db50d78151925bc173
Harold De Leon <harold.dleon@hotmail.com>
2018-11-29 17:12:58 -0500

    Updated Firebase object classes

--- LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/PostActivity.java
+++ LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/PostActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.*;
@@ -16,6 +18,7 @@ import java.util.Random;
 public class PostActivity extends AppCompatActivity implements ValueEventListener {
 
 	private DatabaseReference database;
+	private GoogleSignInAccount account;
 
 	private TextInputEditText title_input;
 	private TextInputEditText author_input;
@@ -31,12 +34,12 @@ public class PostActivity extends AppCompatActivity implements ValueEventListene
 		setSupportActionBar(toolbar);
 
 		database = FirebaseDatabase.getInstance().getReference();
+		account = GoogleSignIn.getLastSignedInAccount(this);
 
 		title_input = findViewById(R.id.editText_titleInput);
 		desc_input = findViewById(R.id.editText_descInput);
 		author_input = findViewById(R.id.editText_authorInput);
 		price_input = findViewById(R.id.editText_priceInput);
-
 
 	}
 
@@ -55,12 +58,13 @@ public class PostActivity extends AppCompatActivity implements ValueEventListene
 		Random r = new Random();
 
 		// TODO: Replace by actual values after Login has been integrated.
-		int post_id = r.nextInt(100);
-		int u_id = r.nextInt(10);
+		final int post_id = r.nextInt(100);
 
 		String author = author_input.getText().toString();
 		String title = title_input.getText().toString();
 		String desc = desc_input.getText().toString();
+		final String u_id = account.getId();
+
 		Double price;
 		try {
 			price = Double.parseDouble(price_input.getText().toString());
@@ -69,12 +73,15 @@ public class PostActivity extends AppCompatActivity implements ValueEventListene
 			price = 0.0;
 		}
 		Post post = new Post(Integer.toString(post_id), author, title, desc, price);
-		database.child("posts/" + u_id + "/" + post_id)
+
+		database.child("posts").push()
 				.setValue(post)
 				.addOnSuccessListener(new OnSuccessListener<Void>() {
 					@Override
 					public void onSuccess(Void aVoid) {
 						Toast.makeText(getBaseContext(), "Post successful", Toast.LENGTH_SHORT).show();
+						UserPost userPost = new UserPost(u_id, Integer.toString(post_id));
+						database.child("user-posts/" + u_id).push().setValue(userPost);
 					}
 				})
 				.addOnFailureListener(new OnFailureListener() {
@@ -84,6 +91,7 @@ public class PostActivity extends AppCompatActivity implements ValueEventListene
 					}
 				});
 	}
+
 
 	private void sendToast(String message) {
 		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();

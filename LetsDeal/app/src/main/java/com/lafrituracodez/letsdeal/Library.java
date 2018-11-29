b2a3d31915dfde0e2d36dee791dda6a7e11ae2f4
Harold De Leon <harold.dleon@hotmail.com>
2018-11-28 20:49:53 -0500

    Reformatted code and removed unused variables.

--- LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/Library.java
+++ LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/Library.java
@@ -2,36 +2,38 @@ package com.lafrituracodez.letsdeal;
 
 public class Library {
 
-    private String book_title;
-    private String book_info;
-    private int book_imageRes;
-    private String book_price;
+	private String book_title;
+	private String book_info;
+	private int book_imageRes;
+	private String book_price;
 
-    public Library(String bookTitle, String title, String info, String price, int resourceId) {
-        this.book_title = title;
-        this.book_info = info;
-        this.book_price = price;
-    }
+	public Library(String bookTitle, String title, String info, String price, int resourceId) {
+		this.book_title = title;
+		this.book_info = info;
+		this.book_price = price;
+	}
 
-    public Library(String title, String info, String price, int imgRes) {
-        this.book_title = title;
-        this.book_info = info;
-        this.book_price = price;
-        book_imageRes = imgRes;
-    }
+	public Library(String title, String info, String price, int imgRes) {
+		this.book_title = title;
+		this.book_info = info;
+		this.book_price = price;
+		book_imageRes = imgRes;
+	}
 
-    public String getTitle() {
-        return book_title;
-    }
+	public String getTitle() {
+		return book_title;
+	}
 
-    public String getInfo() {
-        return book_info;
-    }
+	public String getInfo() {
+		return book_info;
+	}
 
-    public int getImageId() {
-        return book_imageRes;
-    }
+	public int getImageId() {
+		return book_imageRes;
+	}
 
-    public String getPrice() { return book_price; }
+	public String getPrice() {
+		return book_price;
+	}
 
 }
--- LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/MainActivity.java
+++ LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/MainActivity.java
@@ -1,87 +1,83 @@
 package com.lafrituracodez.letsdeal;
 
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.support.design.bottomappbar.BottomAppBar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity  {
-    private int selected = -1;
-    private RecyclerView recyclerView;
-    private static ArrayList<Library> libraryListData;
-    private LibraryAdapter libraryAdapter;
-    private FloatingActionButton addItems; // Will need to go back and use this FAB to add items
-    private int gridColCount = 1;
-   // private TextView aRandomfact;
-   // private ImageView expandRandom;
+public class MainActivity extends AppCompatActivity {
+	private static ArrayList<Library> libraryListData;
+	//private ArrayList<Library> item; // new
+	BottomAppBar bottomAppBar; //bottom appBar code
+	private int selected = -1;
+	private RecyclerView recyclerView;
+	private LibraryAdapter libraryAdapter;
+	private FloatingActionButton addItems; // Will need to go back and use this FAB to add items
+	// private TextView aRandomfact;
+	// private ImageView expandRandom;
 
-    //protected static final String I_AM_HOME= "com.example.I_AM_HOME";
+	//protected static final String I_AM_HOME= "com.example.I_AM_HOME";
+	private int gridColCount = 1;
 
-    //private ArrayList<Library> item; // new
-    BottomAppBar bottomAppBar; //bottom appBar code
+	public static ArrayList<Library> getVariable() {
+		return libraryListData;
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	// fix this before moving on
 
-//        item = new ArrayList<>(); // new
-        recyclerView = findViewById(R.id.recycler_view);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		recyclerView = findViewById(R.id.recycler_view);
 
-        //recyclerView.setLayoutManager(new GridLayoutManager(this, gridColCount)); // LOOK HERE FOR FINAL PROJECT !
+		//recyclerView.setLayoutManager(new GridLayoutManager(this, gridColCount)); // LOOK HERE FOR FINAL PROJECT !
 
-        LinearLayoutManager layoutManager
-                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+		LinearLayoutManager layoutManager
+				= new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
 
 
-        RecyclerView myList = (RecyclerView) findViewById(R.id.recycler_view);
+		RecyclerView myList = findViewById(R.id.recycler_view);
 
 //        myList.setLayoutManager(layoutManager);
 
-        libraryListData = new ArrayList<>();  // LOOK HERE FOR FINAL PROJECT !
-        libraryAdapter = new LibraryAdapter(this, libraryListData);  // LOOK HERE FOR FINAL PROJECT !
-        myList.setAdapter(libraryAdapter);  // LOOK HERE FOR FINAL PROJECT !
-        loadlibraryData();
+		libraryListData = new ArrayList<>();  // LOOK HERE FOR FINAL PROJECT !
+		libraryAdapter = new LibraryAdapter(this, libraryListData);  // LOOK HERE FOR FINAL PROJECT !
+		myList.setAdapter(libraryAdapter);  // LOOK HERE FOR FINAL PROJECT !
+		loadLibraryData();
 
-        bottomAppBar = findViewById(R.id.bottom_appbar); // BottomAppBarCode
-        bottomAppBar.replaceMenu(R.menu.bottom_menu); // BottomAppBarCode
+		bottomAppBar = findViewById(R.id.bottom_appbar); // BottomAppBarCode
+		bottomAppBar.replaceMenu(R.menu.bottom_menu); // BottomAppBarCode
 
-    }
+	}
 
-    // fix this before moving on
+	private void loadLibraryData() {    // LOOK HERE FOR FINAL PROJECT !
+		libraryListData.clear();
 
-    private void loadlibraryData() {    // LOOK HERE FOR FINAL PROJECT !
-        libraryListData.clear();
+		TypedArray libraryImages = getResources().obtainTypedArray(R.array.book_images);
+		String[] libraryTitles = getResources().getStringArray(R.array.book_names);
+		String[] libraryInfos = getResources().getStringArray(R.array.book_description);
+		String[] libraryPrices = getResources().getStringArray(R.array.book_prices);
 
-        TypedArray libraryImages =  getResources().obtainTypedArray(R.array.book_images);
-        String [] libraryTitles = getResources().getStringArray(R.array.book_names);
-        String [] libraryInfos =  getResources().getStringArray(R.array.book_description);
-        String [] libraryPrices =  getResources().getStringArray(R.array.book_prices);
+		for (int i = 0; i < libraryImages.length(); i++) {
+			Library currentBook = new Library(
+					libraryTitles[i],
+					libraryInfos[i],
+					libraryPrices[i],
+					libraryImages.getResourceId(i, 0));
 
-        for(int i=0; i<libraryImages.length(); i++){
-            Library currentBook = new Library(
-                    libraryTitles[i],
-                    libraryInfos[i],
-                    libraryPrices[i],
-                    libraryImages.getResourceId(i, 0));
+			libraryListData.add(currentBook);
 
-            libraryListData.add(currentBook);
+		}
+		libraryAdapter.notifyDataSetChanged();
+		libraryImages.recycle();
 
-        }
-        libraryAdapter.notifyDataSetChanged();
-        libraryImages.recycle();
-
-    }
-
-
-    public static ArrayList<Library> getVariable()
-    {
-        return libraryListData;
-    }
+	}
 
 }

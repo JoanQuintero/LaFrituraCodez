6dd2bea0cc5c270b7fdb6732dbcea81fdc3c70e3
Joan S. Quintero <JoanSQuintero@gmail.com>
2018-11-29 07:06:07 -0500

    updated recyclerview
    added gridlayout for the onclick to view recent posts

--- LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/MainActivity.java
+++ LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/MainActivity.java
@@ -20,9 +20,10 @@ public class MainActivity extends AppCompatActivity {
 	private FloatingActionButton addItems; // Will need to go back and use this FAB to add items
 	// private TextView aRandomfact;
 	// private ImageView expandRandom;
+	private TextView viewRecents;
 
 	//protected static final String I_AM_HOME= "com.example.I_AM_HOME";
-	private int gridColCount = 1;
+	private int recents_gridColCount = 2;
 
 	public static ArrayList<Library> getVariable() {
 		return libraryListData;
@@ -43,17 +44,28 @@ public class MainActivity extends AppCompatActivity {
 				= new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
 
 
-		RecyclerView myList = findViewById(R.id.recycler_view);
+		//RecyclerView myList = findViewById(R.id.recycler_view);
 
-//        myList.setLayoutManager(layoutManager);
+	        recyclerView.setLayoutManager(layoutManager);
 
 		libraryListData = new ArrayList<>();  // LOOK HERE FOR FINAL PROJECT !
 		libraryAdapter = new LibraryAdapter(this, libraryListData);  // LOOK HERE FOR FINAL PROJECT !
-		myList.setAdapter(libraryAdapter);  // LOOK HERE FOR FINAL PROJECT !
+		recyclerView.setAdapter(libraryAdapter);  // LOOK HERE FOR FINAL PROJECT !
 		loadLibraryData();
 
 		bottomAppBar = findViewById(R.id.bottom_appbar); // BottomAppBarCode
 		bottomAppBar.replaceMenu(R.menu.bottom_menu); // BottomAppBarCode
+		
+		viewRecents = findViewById(R.id.recentPost_viewall);
+
+        	viewRecents.setOnClickListener(new View.OnClickListener() {
+            		@Override
+            		public void onClick(View v) {
+               		recyclerView.getRecycledViewPool().clear();
+                	recyclerView.setLayoutManager(
+                        new GridLayoutManager(getApplicationContext(), recents_gridColCount));
+           		 }
+       		 });
 
 	}
 

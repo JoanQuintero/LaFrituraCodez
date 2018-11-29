766a4a1f324f7bf7e1fb7b1b4ea16c80455edc4d
Harold De Leon <harold.dleon@hotmail.com>
2018-11-29 04:23:36 -0500

    Removed unnecessary FAB.

--- LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/PostActivity.java
+++ LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/PostActivity.java
@@ -32,14 +32,6 @@ public class PostActivity extends AppCompatActivity implements ValueEventListene
 		Toolbar toolbar = findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 
-		FloatingActionButton fab = findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
-			}
-		});
 		database = FirebaseDatabase.getInstance().getReference();
 		post_ref = database.child("users");
 

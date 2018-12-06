7e1530420ec067daf79b8959817429bd50bd314b
Harold De Leon <harold.dleon@hotmail.com>
2018-12-06 00:36:04 -0500

    Removed hints, initial support for intents

--- LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/PostAdapter.java
+++ LetsDeal/app/src/main/java/com/lafrituracodez/letsdeal/PostAdapter.java
@@ -4,6 +4,7 @@ package com.lafrituracodez.letsdeal;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -64,14 +65,11 @@ public class PostAdapter extends
 			textPrice = itemView.findViewById(R.id.tv_price);
 			imageViewBook = itemView.findViewById(R.id.imageView_book);
 
-			// itemView.setOnClickListener(this); //The method that will be implemented here is created below as OnClick()
-			//itemView.setOnLongClickListener((View.OnLongClickListener) this); //Find this method as OnLongClick()
+			 itemView.setOnClickListener(this);
+//			itemView.setOnLongClickListener((View.OnLongClickListener) this); //Find this method as OnLongClick()
 		}
 
-		void bindItem(Post currentBook) { //This method puts together the following items
-			// so that they can be shown together... so basically, each cardview has its own
-			// or you can also understand it by saying that each of this item correspond to each other
-			// so they belong in a cardview together.
+		void bindItem(Post currentBook) {
 
 			// TODO: Upload a default key+file.
 			textTitle.setText(currentBook.getTitle());
@@ -84,74 +82,44 @@ public class PostAdapter extends
 
 		}
 
-		//listeners on an item if we want to do some magic
 		@Override
-		public void onClick(View view) { //This is only a normal click
-			// this normal click will send the user to the details activity.
-			// In the details activity you are suppose to pass in the
-			// Title, Description, Image, Calories and a link to an online recipe for the foodie.
+		public void onClick(View view) {
 
 			Post currentBook = postData.get(getAdapterPosition());
-			// the reason why we are creating currFoodie is because we need some info from it.
-			// the currFoodie that will provide this information is the item being clicked because
-			// getAdapterPosition is given a value based on the click.
 
-			bookSelected = getLayoutPosition();
-			//The reason selectedI is here is because the Index of the image needs to be passed as
-			// well because we need it to see which of the elements from the array_list (foodie_details)
-			// will show at the Details Activity, so with the index, we can choose it without a problem.
+			Intent intent = new Intent(context, DetailsActivity.class);
+			intent.putExtra("title", currentBook.getTitle());
+			intent.putExtra("desc", currentBook.getDesc());
+			intent.putExtra("price", currentBook.getPrice());
 
-			//Intent intent = new Intent(context, DetailsActivity.class);
-			//intent.putExtra("IMAGE_ID", currFoodie.getImageId());
-			//intent.putExtra("TITLE", currFoodie.getTitle());
-			//intent.putExtra("INFO",currFoodie.getInfo());
-			//intent.putExtra("INDEX", String.valueOf(selectedI));
-
-
-			//context.startActivity(intent); //finally start the intent so we can retrieve the items.
+			context.startActivity(intent);
 		}
 
 		@Override
-		public boolean onLongClick(View v) { // Action: if an image is long clicked, edit Item will be called.
-			editItem(); // Look for edit Item to see what is happening there.
+		public boolean onLongClick(View v) {
+			editItem();
 			return true;
 		}
 
-		public void editItem() { // This is the function that will be called every time an item is
-			// long clicked.
-			// this method is to create an alert dialog with 2 alert dialog buttons
-			// a "Remove" and a "Cancel"
+		public void editItem() {
 
 			AlertDialog.Builder edit = new AlertDialog.Builder(context);
 			edit.setPositiveButton(
 					"Remove", new DialogInterface.OnClickListener() {
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
-							// removeItem(FoodieData.indexOf(FoodieData.get(getAdapterPosition())));
-							// the remove item being called is asking for the actual position
-							// FoodieData only provides finding an item's index (which is what we need)
-							// but you need to let FoodieData know what Item was it that was clicked and
-							// you do that by getting the getadapterposition which will provide that info
 						}
 					});
 
 			edit.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 				@Override
 				public void onClick(DialogInterface alertSet, int which) {
-					alertSet.dismiss(); // this will dismiss the alert dialog and nothing happens.
+					alertSet.dismiss();
 				}
 			});
 
-			edit.show(); // it is important to show the alert dialog,
-			// otherwise long click on the item will do nothing.
+			edit.show();
 		}
-
-		public void removeItem(int position) { //There's nothing that should be said about what is going on here
-			// item at the position passed in will be removed and the adapter will be notified of it.
-			// FoodieData.remove(position);
-			// notifyItemRemoved(position);
-		}
-
 	}
 
 
